fa2fe08865daabc2e89d12fd0b9adc6b1fd1fd54
Balram Patidar <pbalram088@gmail.com>
2022-06-13 22:48:59 +0530

    first commit
    npm install

---
+++ mart-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      hello summu
+    </div>
+  );
+}
+
+export default App;
